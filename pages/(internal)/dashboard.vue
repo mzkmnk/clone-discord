6b52a47f243bfde657e1bd81b87fb29b0e9f92ab
mzkmnk <sma0729mex@gmail.com>
2024-11-23 18:17:36 +0900

    feat:chat作成

--- pages/(internal)/dashboard.vue
+++ pages/(internal)/dashboard.vue
@@ -2,6 +2,7 @@
 
 import Sidebar from '~/components/(sidebar)/sidebar.vue'
 import {useServersStore} from "~/composables/store/servers.store";
+import Chat from "~/components/dashboard/chat.vue";
 
 const serversStore = useServersStore();
 
@@ -16,8 +17,18 @@ onMounted(async () => {
 <template>
   <div class="flex min-h-screen w-screen">
     <Sidebar />
-    <div class="flex-1">
-      <p>dashboard</p>
+    <div class="flex-1 h-screen relative">
+      <chat class="overflow-y-auto h-[95%]"></chat>
+      <div class="h-[5%] w-full flex items-start justify-center">
+        <div class="cursor-text w-[99%] mx-auto flex items-center justify-center bg-gray-200 h-5/6 rounded-xl px-2 gap-2">
+          <textarea class="resize-none w-full  bg-gray-200 text-slate-800 text-lg focus:outline-none" placeholder="メッセージへ送信" rows="1"></textarea>
+          <Icon
+              class="bg-slate-700 cursor-pointer rounded-full"
+              name="material-symbols:arrows-more-up-rounded"
+              size="20"
+          ></Icon>
+        </div>
+      </div>
     </div>
   </div>
 </template>
